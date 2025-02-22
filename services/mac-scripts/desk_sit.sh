9e196561a0479307c2c0ce761d9d4c6e9f09b313
J-F Poulin <me@lemisieur.com>
2025-02-22 23:44:33 +0000

    feat: add mac scripts for Bluetooth control and desk positioning

---
+++ services/mac-scripts/desk_sit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+osascript <<EOD
+tell application "Desk Controller"
+    move to "75.5cm"
+end tell
+
+EOD
---
+++ services/mac-scripts/desk_stand.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+osascript <<EOD
+tell application "Desk Controller"
+    move to "111.9cm"
+end tell
+
+EOD
