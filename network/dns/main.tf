f77e38a67b25e6af956f268670c73f0db488eaca
J-F Poulin <me@lemisieur.com>
2025-03-13 23:15:40 +0000

    feat: implement dynamic DNS record management with JSON configuration

--- network/dns/main.tf
+++ network/dns/main.tf
@@ -1,6 +1,14 @@
-data unifi_dns_record "dns_record" {
-  name    = "lab.lemisieur.com"
+locals {
+  dns_records = jsondecode(file("${path.module}/records.json"))
 }
-output "dns_test" {
-  value = data.unifi_dns_record.dns_record
+resource "unifi_dns_record" "a" {
+  for_each = { for record in local.dns_records.A : record.name => record }
+
+  name        = each.value.name
+  record_type = "A"
+  value       = each.value.value
+  enabled     = true
+  port        = 0
+  priority    = 0
+  ttl         = 0
 }
